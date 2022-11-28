c537b4dac2707c7e03994ecfbaab9a7cd98cdfc3
Breno Araujo <breno.souza.araujo@hotmail.com>
2022-11-27 21:07:15 -0300

    Led

--- Arduino-servomotor-sensor-hc.ino
+++ Arduino-servomotor-sensor-hc.ino
@@ -6,15 +6,25 @@ Servo servoMotor;
 int trigPin = 7;  // trigger pin on 7
 int echoPin = 8;  // echo pin on 8
 
-int pos = 0;
+
+int redLed = 10;
+int yellowLed = 11;
+
 
 double distance = 0;
+
+
 void setup() {
   Serial.begin(9600);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 
   servoMotor.attach(3);
+
+
+
+  digitalWrite(redLed, LOW);
+  digitalWrite(yellowLed, LOW);
 }
 
 void loop() {
@@ -33,22 +43,30 @@ void loop() {
   Serial.println(" cm");
 
 
-  if (distance <= 10.00 && distance !=0.0) {
-    for(pos; pos<=90; pos ++){
-      servoMotor.write(pos);
 
+  if (distance <= 10.00 && distance != 0.0) {
+
+    servoMotor.write(90);
+
+
+
+    for (int i = 0; i <= 30; i++) {
+      digitalWrite(redLed, HIGH);  // turn on red
+      delay(100);
+      digitalWrite(redLed, LOW);  // turn off red
+      digitalWrite(yellowLed, HIGH);  // turn on yellow
+      delay(100);
+      digitalWrite(yellowLed, LOW);  // turn off yellow
     }
 
-    delay(5000);
 
-      for(pos; pos>=0; pos--){
-      servoMotor.write(pos);
-      delay(25);
-    }
-    
+
+
+  } else {
+    servoMotor.write(0);
   }
 
 
-  delay(2000);
-
+  digitalWrite(redLed, LOW);     // turn on red
+  digitalWrite(yellowLed, LOW);  // turn off yellow
 }
